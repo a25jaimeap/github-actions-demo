7aec2f6f450ad8f6e07c988a6d38923c7ac53137
Jaime <a25jaimeap@iessanclemente.net>
2026-04-22 16:13:05 +0200

    actualizado

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 import math
+
+
 def is_prime(n):
-	if n < 0:
-		return "Os números negativos non están permitidos"
-	if n <= 1:
-		return False
-	if n == 2:
-		return True
-	if n % 2 == 0:
-		return False
-	for i in range(2, int(math.sqrt(n)), 1):
-		if n % i == 0:
-			return False
-	return True
+    if n < 0:
+        return "Os números negativos non están permitidos"
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
